5743630f6999ef80025866622f8fbea8ffd92474
hossein <149826811+hossein1dr@users.noreply.github.com>
2025-08-12 11:01:29 +0330

    add(file):i add my exam

---
+++ exam-one/exam-one/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_one
+{
+    class leftPointer
+    {
+        private int numberOfRow;
+        public leftPointer(int number)
+        {
+            numberOfRow = number;
+        }
+        public void showLeftPointer()
+        {
+            for (int i = 0; i < numberOfRow; i++)
+            {
+
+                for (int j = 0; j <= numberOfRow - i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls enter number");
+            int x=int.Parse(Console.ReadLine());
+          leftPointer newleftPointer=new leftPointer(x);
+            newleftPointer.showLeftPointer();
+            
+            Console.ReadKey();
+
+        }
+    }
+}
